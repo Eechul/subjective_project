f06ff9008f26eae347b6260c1e554bad5c933013
edongchel <choise154@gmail.com>
2016-07-30 10:19:58 +0900

    회원가입 예외처리 (3/4)

---
+++ config/userFunction/dateFormat.js
@@ -0,0 +1,10 @@
+
+
+Date.prototype.format = function( )
+{
+  var yyyy = this.getFullYear().toString();
+  var mm = (this.getMonth()+1).toString();
+  var dd = this.getDate().toString();
+
+  return yyyy + (mm[1] ? mm : '0'+mm[0]) + (dd[1] ? dd : '0' +dd[0])
+}
