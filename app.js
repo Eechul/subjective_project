6c82e68cf084a0a474f883f6fb0bb4a6ad1dd5c6
edongchel <choise154@gmail.com>
2016-08-04 00:01:23 +0900

    로그인 passport (session) 추가 (1/2)

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var session = require('express-session');
 var MySQLStore = require('express-mysql-session')(session);
 var app = express();
 var dateFormat = require('./config/userFunction/dateFormat')
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy;
 app.use(session({
   secret: '123215415@@DSAGnklndklsa',
   resave: false,
@@ -17,13 +19,19 @@ app.use(session({
     database  : 'webquizdb'
   })
 }));
+passport.use(new LocalStrategy({
+  function(username, password, done) {
+    // 세션 등록할 유저 --
+  }
+}));
+
 app.use(express.static('public'));
 app.set('views', __dirname+'/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
 app.use(bodyParser.urlencoded({ extended: false}))
-var home = require('./routes/home')();
+var home = require('./routes/home')(passport);
 app.use('/home', home);
 
 app.get('/home', function(req, res) {
