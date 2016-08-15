0bee3345d4797938361ff756e36d4f0c610d7963
DESKTOP-TNQO1IK\chois <choise154@gmail.com>
2016-08-15 22:25:56 +0900

    문제 만드는 페이지 작성  - 여러문제 작성(3/5)

--- public/js/quiz/createQuiz/createQuiz.js
+++ public/js/quiz/createQuiz/createQuiz.js
@@ -1,11 +1,19 @@
-var exampleNumber = 1;
-var questionNumber = 2;
-var buttonNumber = 1;
-var sequenceArr = [];
+MYAPP = {}
+MYAPP.questionNumber = 2;
+MYAPP.exampleNumber = 1;
+MYAPP.sequenceArr = new Array(new Array());
+MYAPP.sequenceArr[1] = new Array()
+
 $(document).ready(function(){
+  $("#removeQuestion").click(function(){
+    $(".question:last").remove();
+  })
   $("#addQuestion").click(function(){
-    var question = `<div id="question_${questionNumber}">
+    var questionNumber = MYAPP.questionNumber;
+    MYAPP.sequenceArr[questionNumber] = new Array();
+    var question = `<div id="question_${questionNumber}" class="question">
       <label>${questionNumber}번</label>
+      <button type="button" class="btn btn-lg btn-danger btn-xs" id="removeQuestion">문제삭제</button>
       <div class="container-fluid text-center">
         <label class="radio-inline">
             <input type="radio" id="question_type" name="question_type_${questionNumber}"
@@ -19,54 +27,71 @@ $(document).ready(function(){
       <div id="question_type_load_${questionNumber}"></div>
     </div>
     `
+
     $(".questions").append(question)
-    questionNumber++;
+    MYAPP.questionNumber++
   });
 
   $("#saveQuestion").click(function() {
-    // 문제점: number가 커지면 포문이 많이돌게됨..
-  //   for(var i=1; i<number; i++) {
-  //     if($("#exampleText_"+i)){
-  //       console.log(i+') '+$("#exampleText_"+i).val());
-  //     }
-  //   }
+
   })
   $("body").on("change", "input[id=question_type]", function() {
+    // TODO 객관식 -> 주관식, 주관식->객관식 타입 바뀌면 보기 문항 초기화 (경고표시 띄워주기)
+
     var questionNumber = $(this).val().split('_')[2]
-    console.log(questionNumber);
     if($(this).val() == 'multiple_choice_'+questionNumber) {
-      $('#question_type_load_'+questionNumber).load('/ajax/loadCreateQuiz.ejs #multiple_choice')
+      MYAPP.sequenceArr[questionNumber] = new Array()
+      $('#short_answer_'+questionNumber).remove()
+      $('#question_type_load_'+questionNumber).append(`
+        <div id="multiple_choice_${questionNumber}" class="container-fluid">
+          <div class="">
+              <label for="객관식">객관식</label>
+              <textarea class="form-control" rows="5" id="comment" placeholder="문제입력"></textarea>
+          </div>
+          <div class="container-fluid">
+            <div id="examples_${questionNumber}"></div>
+            <button type="button" id="addExample" class="btn btn-lg btn-info btn-xs" >보기추가</button>
+          </div>
+        </div>
+        `);
     }
     else if($(this).val() == 'short_answer_'+questionNumber) {
-      $('#question_type_load_'+questionNumber).load('/ajax/loadCreateQuiz.ejs #short_answer')
+      MYAPP.sequenceArr[questionNumber] = new Array()
+      $('#multiple_choice_'+questionNumber).remove()
+      $('#question_type_load_'+questionNumber).append(`
+        <div id="short_answer_${questionNumber}" class="container-fluid">
+                <div class="form-group">
+                    <label for="comment">주관식</label>
+                    <textarea class="form-control" rows="5" id="comment" placeholder="문제입력"></textarea>
+              </div>
+              <div class="form-group">
+                  <label for="답안">답안</label>
+                  <div id="answers"></div>
+              </div>
+              <div class="container-fluid">
+                <button type="button" id="addAnswer" class="btn btn-lg btn-info btn-xs" >유사답안추가</button>
+              </div>
+        </div>
+        `)
     }
   })
-  // $("input[name=question_type]").change(function() {
-  //   if($(this).val() == 'multiple_choice') {
-  //     $('#question_type_load').load('/ajax/loadCreateQuiz.html #multiple_choice')
-  //   }
-  //   else if($(this).val() == 'short_answer') {
-  //     $('#question_type_load').load('/ajax/loadCreateQuiz.html #short_answer')
-  //   }
-  // })
-  //
   $("body").on("click", "#addExample", function() {
+    var questionNumber = this.parentNode.parentNode.id.split('_')[2];
+    //console.log('test2',questionNumber);
     var example =
-    `<div id="example_${exampleNumber}">
-            <label id="number_${exampleNumber}"></label>
-            <input id="exampleAnswer_${exampleNumber}" name="checkAnswer" type="checkbox" />
-            <input id="exampleText_${exampleNumber}" name="exampleContent" type="text">
-            <button type="button" id="removeExample" class="btn btn-lg btn-danger btn-xs">삭제</button>
-    </div>
-    `
-    sequenceArr.push(exampleNumber)
-    var index = sequenceArr.indexOf(Number(exampleNumber))+1
-    console.log(index);
-    $("#examples").append(example);
-    $("#number_"+exampleNumber).text(index)
-    console.log(sequenceArr);
-    exampleNumber++;
-
+      `<div id="example_${MYAPP.exampleNumber}">
+              <label id="number_${MYAPP.exampleNumber}"></label>
+              <input id="exampleAnswer_${MYAPP.exampleNumber}" name="checkAnswer" type="checkbox" />
+              <input id="exampleText_${MYAPP.exampleNumber}" name="exampleContent" type="text">
+              <button type="button" id="removeExample" class="btn btn-lg btn-danger btn-xs">삭제</button>
+      </div>`
+    MYAPP.sequenceArr[questionNumber].push(MYAPP.exampleNumber)
+    var index = MYAPP.sequenceArr[questionNumber].indexOf(Number(MYAPP.exampleNumber))+1
+    console.log("2차원",MYAPP.sequenceArr);
+    $("#examples_"+questionNumber).append(example);
+    $("#number_"+MYAPP.exampleNumber).text(index)
+    console.log(MYAPP.sequenceArr);
+    MYAPP.exampleNumber++;
   });
   $("body").on("click", "#removeExample", function() {
     //1. '삭제'버튼의 부모노드는 찾는다.
@@ -76,26 +101,23 @@ $(document).ready(function(){
     //5. 부모노드를 삭제
     //6. 삭제고 난 후의 sequenceArr 배열에 맞게 순서 정렬
     //=> index: 보기의 번호, 값: 태그의 아이디값
-    var exampleId = this.parentNode.id
-    var splitStr = exampleId.split('_')[1]
-    var exampleIndex= sequenceArr.indexOf(Number(splitStr))
+    // (수정) 자료구조 = 1차원 배열 -> 2차원 배열
+    var questionNumber = this.parentNode.parentNode.id.split('_')[1]
+    var exampleId  = this.parentNode.id.split('_')[1]
+    var exampleIndex= MYAPP.sequenceArr[questionNumber].indexOf(Number(exampleId))
     console.log(exampleIndex);
-    sequenceArr.splice(exampleIndex,1);
-    console.log(sequenceArr);
-      this.parentNode.remove();
-    for(var i=exampleIndex; i<sequenceArr.length; i++) {
-      $("#number_"+sequenceArr[i]).text(i+1)
+    MYAPP.sequenceArr[questionNumber].splice(exampleIndex,1);
+    console.log(MYAPP.sequenceArr);
+    this.parentNode.remove();
+    for(var i=exampleIndex; i<MYAPP.sequenceArr[questionNumber].length; i++) {
+      $("#number_"+MYAPP.sequenceArr[questionNumber][i]).text(i+1)
     }
   });
+
   $("body").on("focus", "input[name=exampleContent]", function() {
     $(this).attr('size','50')
   });
   $("body").on("blur", "input[name=exampleContent]", function() {
     $(this).removeAttr('size')
   });
- //  $("input[name=exampleContent]").focus(function(){
- //    alert('gogo')
- // })
-
-
 })
